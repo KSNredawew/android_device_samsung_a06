dc1290a0dc3ae7014cae768887c0f1f6a2b0a141
KSN <redawewrq12@gmail.com>
2025-07-15 14:09:55 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-5 Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 The Android Open Source Project
 # Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
@@ -9,6 +9,12 @@ LOCAL_PATH := device/samsung/a06
 
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-impl-default \
+    android.hardware.health@2.0-impl-2.1 \
+    libhealthd.$(PRODUCT_PLATFORM)
 
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
