7b8a6d52222a182885e64b5bb7ed6d7dcfba74c4
KSN <redawewrq12@gmail.com>
2025-07-15 13:58:31 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -10,12 +10,6 @@ LOCAL_PATH := device/samsung/a06
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl-default \
-    android.hardware.health@2.0-impl-2.1 \
-    libhealthd.$(PRODUCT_PLATFORM)
-
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
